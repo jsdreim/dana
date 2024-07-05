c52bac485c51513ee58edd1bad74adafd3ac6727
J.S. Dreim <jsdreim@protonmail.com>
2024-07-05 13:21:31 -0400

    Implement new macro to use fewer macros

--- src/units/concrete.rs
+++ src/units/concrete.rs
@@ -1,46 +1,24 @@
 use super::traits::*;
 
-pub mod distance;
-pub mod energy;
-pub mod force;
-pub mod mass;
-pub mod time;
 
-pub use distance::Distance;
-pub use energy::Energy;
-pub use force::Force;
-pub use mass::Mass;
-pub use time::Time;
+macro_rules! concrete_types {
+    ($($module:ident::$unit:ident),+$(,)?) => {
+        $(
+        pub mod $module;
+        pub use $module::$unit;
+        )+
 
+        impl_unit_ops!($($unit),+);
+        impl_unit_pow!($($unit),+);
+        impl_unit_pow_n!($($unit),+);
+        impl_unit_concrete!($($unit),+);
+    };
+}
 
-impl_unit_concrete!(
-    Distance,
-    Energy,
-    Force,
-    Mass,
-    Time,
-);
-
-impl_unit_ops!(
-    Distance,
-    Energy,
-    Force,
-    Mass,
-    Time,
-);
-
-impl_unit_pow!(
-    Distance,
-    Energy,
-    Force,
-    Mass,
-    Time,
-);
-
-impl_unit_pow_n!(
-    Distance,
-    Energy,
-    Force,
-    Mass,
-    Time,
+concrete_types!(
+    distance::Distance,
+    energy::Energy,
+    force::Force,
+    mass::Mass,
+    time::Time,
 );
