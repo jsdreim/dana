ec68979ffa4fd07cfd0b7b1e6fb1b8b65b3acb97
J.S. Dreim <jsdreim@protonmail.com>
2024-07-25 23:20:16 -0400

    Revise benchmark

--- benches/qty_simd.rs
+++ benches/qty_simd.rs
@@ -49,26 +49,27 @@ fn bench_simd_group<V, const N: usize, S>(c: &mut Criterion, name: &str) where
     >
 {
     let mut group = c.benchmark_group(name);
+    group.sample_size(500);
 
     let rng = &mut rand::thread_rng();
     let pos: [V; N] = from_fn(|_| rng.gen_range( 1.0..=20.0).as_());
     let vel: [V; N] = from_fn(|_| rng.gen_range(-2.0..= 2.0).as_());
     let time: V     =             rng.gen_range( 1.0..= 5.0).as_();
 
-    let pos_f: Simd<V, { N }> = Simd::from(pos);
-    let vel_f: Simd<V, { N }> = Simd::from(vel);
-    let time_f: Simd<V, { N }> = Simd::from([time; N]);
+    let pos_n: Simd<V, { N }> = Simd::from(pos);
+    let vel_n: Simd<V, { N }> = Simd::from(vel);
+    let time_n: Simd<V, { N }> = Simd::from([time; N]);
 
     let pos_q: QtySimd<Length, V, { N }, S> = m.quantity_simd(pos);
     let vel_q: QtySimd<Speed, V, { N }, S> = (m/s).quantity_simd(vel);
     let time_q: QtySimd<Time, V, { N }, S> = s.quantity(time).to_simd();
 
     group.bench_function(
-        "float",
+        "native",
         |b| b.iter(|| simd_float(
-            black_box(pos_f),
-            black_box(vel_f),
-            black_box(time_f),
+            black_box(pos_n),
+            black_box(vel_n),
+            black_box(time_n),
         )),
     );
 
@@ -85,14 +86,15 @@ fn bench_simd_group<V, const N: usize, S>(c: &mut Criterion, name: &str) where
 
 fn bench_simd(c: &mut Criterion) {
     macro_rules! bench {($c:ident, $ty:ty, $($n:literal),+ $(,)?) => {$(
-        bench_simd_group::<$ty, $n, $ty>($c, concat!("SIMD/", stringify!($ty), "x", $n));
+        bench_simd_group::<$ty, $n, $ty>($c, concat!("SIMD-", stringify!($ty), "x", $n));
     )+}}
 
     // bench!(c, f32, 1, 2, 4, 8, 16, 32);
     // bench!(c, f64, 1, 2, 4, 8, 16, 32);
 
-    bench!(c, f32, 2, 4);
-    bench!(c, f64, 1, 2);
+    bench!(c, f32, /*2,*/ 4, /*8*/);
+    bench!(c, f64, /*1,*/ 2, /*4*/);
+    bench!(c, i32, /*2,*/ 4, /*8*/);
 }
 
 
