8ed198b94683832581a6cdc912e49301ffe9ffd1
J.S. Dreim <jsdreim@protonmail.com>
2024-08-08 18:23:11 -0400

    Move exponential unit traits into submodule

--- src/units/traits.rs
+++ src/units/traits.rs
@@ -7,6 +7,9 @@ use crate::{
     Value,
 };
 
+mod exponential;
+pub use exponential::*;
+
 pub mod transform;
 pub use transform::*;
 
@@ -165,6 +168,7 @@ pub trait UnitMixed: Unit {
 ///     mathematical relationship between them.
 pub trait UnitCompound: Unit {}
 
+
 /// A "concrete" unit is irreducible, and typically corresponds directly to a
 ///     physical property. It typically has multiple variants with different
 ///     scales.
@@ -176,84 +180,3 @@ pub trait UnitConcrete: Unit + UnitStep {
     ///     an optional SI scaling prefix.
     fn symbol(&self) -> &'static str;
 }
-
-
-//region Exponential traits.
-//region Whole exponents.
-/// Trait for a type that can be raised to a power.
-pub trait CanPow<const E: i32>: Unit {
-    /// The result of the operation.
-    type Output: Unit;
-
-    /// Raise this unit to the power of the const parameter `E`.
-    fn pow(self) -> Self::Output;
-}
-
-/// Trait for a type that can be raised to the second power.
-pub trait CanSquare: Unit {
-    /// The result of the operation.
-    type Output: Unit;
-
-    /// Return the square of this unit.
-    fn squared(self) -> Self::Output;
-}
-
-impl<U: CanPow<2>> CanSquare for U {
-    type Output = U::Output;
-    fn squared(self) -> Self::Output { self.pow() }
-}
-
-/// Trait for a type that can be raised to the third power.
-pub trait CanCube: Unit {
-    /// The result of the operation.
-    type Output: Unit;
-
-    /// Return the cube of this unit.
-    fn cubed(self) -> Self::Output;
-}
-
-impl<U: CanPow<3>> CanCube for U {
-    type Output = U::Output;
-    fn cubed(self) -> Self::Output { self.pow() }
-}
-//endregion
-
-//region Roots.
-/// Trait for a type that can be taken to an exponential root.
-pub trait CanRoot<const D: i32>: Unit {
-    /// The result of the operation.
-    type Output: Unit;
-
-    /// Take the root of this unit to the degree of the const parameter `D`.
-    fn root(self) -> Self::Output;
-}
-
-/// Trait for a type that can be taken to the second root.
-pub trait CanSquareRoot: Unit {
-    /// The result of the operation.
-    type Output: Unit;
-
-    /// Return the square root of this unit.
-    fn sqrt(self) -> Self::Output;
-}
-
-impl<U: CanRoot<2>> CanSquareRoot for U {
-    type Output = U::Output;
-    fn sqrt(self) -> Self::Output { self.root() }
-}
-
-/// Trait for a type that can be taken to the third root.
-pub trait CanCubeRoot: Unit {
-    /// The result of the operation.
-    type Output: Unit;
-
-    /// Return the cube root of this unit.
-    fn cbrt(self) -> Self::Output;
-}
-
-impl<U: CanRoot<3>> CanCubeRoot for U {
-    type Output = U::Output;
-    fn cbrt(self) -> Self::Output { self.root() }
-}
-//endregion
-//endregion
